6978ecfd857d80f3b302202bdb3885047d146d83
Serkan <serkan-afsar@hotmail.com>
2023-10-22 08:21:33 +0300

    Seo Update

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,38 @@ import CityListContainer from "@/Containers/CityListContainer";
 import HeroSection from "@/Components/HeroSection/HeroSection";
 import ServicesSection from "@/Components/ServicesSection/ServicesSection";
 
+export const metadata = {
+  title: "Türkiye İl - İlçe Taksi Durakları Listesi",
+  description: "Türkiye İl - İlçe Taksi Durakları Listesi",
+  charSet: "UTF-8",
+  robots: "index,follow",
+  publisher: "Taksi Surakları",
+  author: "Taksi Durakları - info@taksi-duraklari.net",
+  distribution: "global",
+  themeColor: "#fff",
+  resourceType: "Web Page",
+  openGraph: {
+    title: "Türkiye İl - İlçe Taksi Durakları Listesi",
+    description: "Türkiye  İl - İlçe Taksi Durakları Listesi",
+    publisher: "Taksi Durakları",
+    locale: "tr_TR",
+    site_name: "Taksi Durakları",
+  },
+  twitter: {
+    card: "summary",
+    description: "Türkiye İl - İlçe Taksi Durakları Listesi",
+    title: "Türkiye İl - İlçe Taksi Durakları Listesi",
+    creator: "@taksiduraklari",
+  },
+  resourceType: "Web Page",
+  alternates: {
+    canonical: `${process.env.SITE_NAME}`,
+    dnsPrefetch: "//fonts.googleapis.com/",
+    dnsPrefetch: "//www.google-analytics.com/",
+    dnsPrefetch: "//ajax.googleapis.com/",
+  },
+};
+
 export default async function Home() {
   return (
     <>
--- app/taksi-duraklari/[[...slug]]/page.jsx
+++ app/taksi-duraklari/[[...slug]]/page.jsx
@@ -4,6 +4,116 @@ import { GetCityList, GetCityDetail } from "@/Services/City.Service";
 import { slugUrl } from "@/Utils/Helpers";
 import TaxiListContainer from "@/Containers/TaxiListContainer";
 
+export async function generateMetadata({ params }, parent) {
+  // read route params
+  const { slug } = params;
+
+  if (!slug) {
+    return {
+      title: "Türkiye İl - İlçe Taksi Durakları Listesi",
+      description: "Türkiye İl - İlçe Taksi Durakları Listesi",
+      charSet: "UTF-8",
+      robots: "index,follow",
+      publisher: "Taksi Surakları",
+      author: "Taksi Durakları - info@taksi-duraklari.net",
+      distribution: "global",
+      themeColor: "#fff",
+      resourceType: "Web Page",
+      openGraph: {
+        title: "Türkiye İl - İlçe Taksi Durakları Listesi",
+        description: "Türkiye  İl - İlçe Taksi Durakları Listesi",
+        publisher: "Taksi Durakları",
+        locale: "tr_TR",
+        site_name: "Taksi Durakları",
+      },
+      twitter: {
+        card: "summary",
+        description: "Türkiye İl - İlçe Taksi Durakları Listesi",
+        title: "Türkiye İl - İlçe Taksi Durakları Listesi",
+        creator: "@taksiduraklari",
+      },
+      resourceType: "Web Page",
+      alternates: {
+        canonical: `${process.env.SITE_NAME}`,
+        dnsPrefetch: "//fonts.googleapis.com/",
+        dnsPrefetch: "//www.google-analytics.com/",
+        dnsPrefetch: "//ajax.googleapis.com/",
+      },
+    };
+  }
+  const { data } = await GetCityDetail({
+    citySlug: slug[0],
+  });
+
+  if (slug.length == 1) {
+    return {
+      title: `${data[0]?.Sehir} Taksi Durakları`,
+      description: `${data[0]?.Sehir} Taksi Durakları`,
+      charSet: "UTF-8",
+      robots: "index,follow",
+      publisher: "Taksi Durakları",
+      author: "Taksi Durakları - info@taksiduraklari.net",
+      distribution: "global",
+      themeColor: "#fff",
+      resourceType: "Web Page",
+      openGraph: {
+        title: `${data[0]?.Sehir} Taksi Durakları`,
+        description: `${data[0]?.Sehir} Taksi Durakları`,
+        publisher: "Taksi Durakları",
+        locale: "tr_TR",
+        site_name: "Taksi Durakları",
+      },
+      twitter: {
+        card: "summary",
+        description: `${data[0]?.Sehir} Taksi Durakları`,
+        title: `${data[0]?.Sehir} Taksi Durakları`,
+        creator: "@taksiduraklari",
+      },
+      resourceType: "Web Page",
+      alternates: {
+        canonical: `${process.env.SITE_NAME}/taksi-duraklari/${slug[0]}`,
+        dnsPrefetch: "//fonts.googleapis.com/",
+        dnsPrefetch: "//www.google-analytics.com/",
+        dnsPrefetch: "//ajax.googleapis.com/",
+      },
+    };
+  }
+  if (slug.length == 2) {
+    const item = data.filter((a) => slugUrl(a.ilce) == slug[1])[0];
+    return {
+      title: `${item?.Sehir} ${item?.ilce} Taksi Durakları`,
+      description: `${item?.Sehir} ${item?.ilce} Taksi Durakları`,
+      charSet: "UTF-8",
+      robots: "index,follow",
+      publisher: "Taksi Durakları",
+      author: "Taksi Durakları - info@taksiduraklari.net",
+      distribution: "global",
+      themeColor: "#fff",
+      resourceType: "Web Page",
+      openGraph: {
+        title: `${item?.Sehir} ${item?.ilce} Taksi Durakları`,
+        description: `${item?.Sehir} ${item?.ilce} Taksi Durakları`,
+        publisher: "Taksi Durakları",
+        locale: "tr_TR",
+        site_name: "Taksi Durakları",
+      },
+      twitter: {
+        card: "summary",
+        description: `${item?.Sehir} ${item?.ilce} Taksi Durakları`,
+        title: `${item?.Sehir} ${item?.ilce} Taksi Durakları`,
+        creator: "@taksiduraklari",
+      },
+      resourceType: "Web Page",
+      alternates: {
+        canonical: `${process.env.SITE_NAME}/taksi-duraklari/${slug[0]}/${slug[1]}`,
+        dnsPrefetch: "//fonts.googleapis.com/",
+        dnsPrefetch: "//www.google-analytics.com/",
+        dnsPrefetch: "//ajax.googleapis.com/",
+      },
+    };
+  }
+}
+
 export default async function Page({ params }) {
   const { slug } = params;
 
