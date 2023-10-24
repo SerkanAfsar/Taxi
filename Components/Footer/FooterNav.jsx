912b232121f02893486aff7bbb3153b352d2460c
Serkan <serkan-afsar@hotmail.com>
2023-10-24 18:20:09 +0300

    SiteMap Init & Footer Nav Init

--- Components/Footer/FooterNav.jsx
+++ Components/Footer/FooterNav.jsx
@@ -17,78 +17,123 @@ export default function FooterNav() {
             <h4>Taksi Durakları</h4>
             <ul>
               <li>
-                <Link href={"/"} title="İstanbul Taksi Durakları">
+                <Link
+                  href={"/taksi-duraklari/istanbul"}
+                  title="İstanbul Taksi Durakları"
+                >
                   İstanbul Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Ankara Taksi Durakları">
+                <Link
+                  href={"/taksi-duraklari/ankara"}
+                  title="Ankara Taksi Durakları"
+                >
                   Ankara Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="İzmir Taksi Durakları">
+                <Link
+                  href={"/taksi-duraklari/izmir"}
+                  title="İzmir Taksi Durakları"
+                >
                   İzmir Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Bursa Taksi Durakları">
+                <Link
+                  href={"/taksi-duraklari/bursa"}
+                  title="Bursa Taksi Durakları"
+                >
                   Bursa Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Eskişehir Taksi Durakları">
+                <Link
+                  href={"/taksi-duraklari/eskisehir"}
+                  title="Eskişehir Taksi Durakları"
+                >
                   Eskişehir Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="İstanbul Taksi Durakları">
-                  İstanbul Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/adana"}
+                  title="Adana Taksi Durakları"
+                >
+                  Adana Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Ankara Taksi Durakları">
-                  Ankara Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/konya"}
+                  title="Konya Taksi Durakları"
+                >
+                  Konya Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="İzmir Taksi Durakları">
-                  İzmir Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/kayseri"}
+                  title="Kayseri Taksi Durakları"
+                >
+                  Kayseri Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Bursa Taksi Durakları">
-                  Bursa Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/kocaeli"}
+                  title="Bursa Taksi Durakları"
+                >
+                  Kocaeli Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Eskişehir Taksi Durakları">
-                  Eskişehir Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/kastamonu"}
+                  title="Kastamonu Taksi Durakları"
+                >
+                  Kastamonu Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="İzmir Taksi Durakları">
-                  İzmir Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/malatya"}
+                  title="Malatya Taksi Durakları"
+                >
+                  Malatya Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Bursa Taksi Durakları">
-                  Bursa Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/elazig"}
+                  title="Elazığ Taksi Durakları"
+                >
+                  Elazığ Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Eskişehir Taksi Durakları">
-                  Eskişehir Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/kilis"}
+                  title="Kilis Taksi Durakları"
+                >
+                  Kilis Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="İzmir Taksi Durakları">
-                  İzmir Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/mersin"}
+                  title="Mersin Taksi Durakları"
+                >
+                  Mersin Taksi Durakları
                 </Link>
               </li>
               <li>
-                <Link href={"/"} title="Bursa Taksi Durakları">
-                  Bursa Taksi Durakları
+                <Link
+                  href={"/taksi-duraklari/mugla"}
+                  title="Muğşa Taksi Durakları"
+                >
+                  Muğla Taksi Durakları
                 </Link>
               </li>
             </ul>
