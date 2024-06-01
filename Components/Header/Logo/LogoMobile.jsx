bd5b2e00717fabb8c92808bc22efa3904b5507f1
SerkanAfsar <serkan-afsar@hotmail.com>
2024-06-01 20:31:03 +0300

    Logo Mobile Removed

--- Components/Header/Logo/LogoMobile.jsx
+++ Components/Header/Logo/LogoMobile.jsx
@@ -3,7 +3,7 @@ import image from "../../../public/images/taksi.webp";
 import styles from "./LogoMobile.module.scss";
 export default function LogoMobile() {
   return (
-    <div className={`d-block d-lg-none ${styles.logoMobile}`}>
+    <div className={`d-none d-lg-none ${styles.logoMobile}`}>
       <Image src={image} width={50} height={60} alt="Taksi Durakları" />
     </div>
   );
