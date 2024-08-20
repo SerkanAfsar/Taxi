1efb1a5fe60732057b7d2bbe207229cf36f6b7ce
Serkan Afşarm <serkan-afsar@hotmail.com>
2024-08-20 13:19:04 +0300

    Social Added

--- Components/CityDetail/CityDetail.jsx
+++ Components/CityDetail/CityDetail.jsx
@@ -1,15 +1,51 @@
+"use client";
 import Districts from "../Districts/Disticts";
-import TaxiDetail from "../TaxiDetail/TaxiDetail";
 import BreadCrumb from "../UI/BreadCrumb/BreadCrumb";
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  LinkedinShareButton,
+  LinkedinIcon,
+  PinterestShareButton,
+  PinterestIcon,
+  TelegramShareButton,
+  TelegramIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  FacebookIcon,
+} from "react-share";
 export default function CityDetail({ breadCrumbArr, item, children }) {
   return (
     <div className="container my-3">
-      <div className="row">
-        <div className="col-12">
-          {/* {breadCrumbArr?.map((item, index) => (
-            <div key={index}>{item.name}</div>
-          ))} */}
+      <div className="row mb-3">
+        <div className="col-12 col-lg-8 d-flex justify-content-center justify-content-lg-between  align-items-center">
           <BreadCrumb arr={breadCrumbArr} />
+        </div>
+        <div className="col-12 col-lg-4 d-inline-flex  flex-wrap gap-2 justify-content-center justify-content-lg-end align-items-center">
+          <FacebookShareButton url={window.location.href}>
+            <FacebookIcon size={32} round={true} />
+          </FacebookShareButton>
+          <EmailShareButton url={window.location.href}>
+            <EmailIcon size={32} round={true} />
+          </EmailShareButton>
+          <LinkedinShareButton url={window.location.href}>
+            <LinkedinIcon size={32} round={true} />
+          </LinkedinShareButton>
+          <WhatsappShareButton url={window.location.href}>
+            <WhatsappIcon size={32} round={true} />
+          </WhatsappShareButton>
+          <TwitterShareButton url={window.location.href}>
+            <TwitterIcon size={32} round={true} />
+          </TwitterShareButton>
+          <PinterestShareButton url={window.location.href}>
+            <PinterestIcon size={32} round={true} />
+          </PinterestShareButton>
+          <TelegramShareButton url={window.location.href}>
+            <TelegramIcon size={32} round={true} />
+          </TelegramShareButton>
         </div>
       </div>
       <div className="row">
